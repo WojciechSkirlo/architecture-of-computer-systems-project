0d111b3395bbbd50f4f597f87a7236f0b19b0e54
Wojciech Skirło <34141085+WojciechSkirlo@users.noreply.github.com>
2022-05-22 11:47:52 +0100

    chnage look

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app" class="flex justify-center items-center w-full text-sm min-h-screen bg-grey-50 p-8">
-    <div class="container max-w-screen-xl p-4 rounded bg-neutral-200 flex gap-6">
-      <div class="flex flex-col w-1/2 bg-white p-4 rounded">
+  <div id="app" class="flex justify-center items-center w-full text-sm min-h-screen bg-grey-50 p-4 md:p-8">
+    <div class="container justify-center max-w-screen-xl p-4 rounded bg-neutral-200 flex gap-6">
+      <div class="flex flex-col w-full xl:w-1/2 bg-white p-4 rounded">
         <div class="flex justify-center gap-x-6 items-center mb-6">
-          <BaseButton>restart</BaseButton>
+          <BaseButton @click="restart">restart</BaseButton>
           <BaseButton>random</BaseButton>
         </div>
         <div class="flex items-center gap-x-4 mb-2">
-          <span>AX</span>
+          <span class="w-5 shrink-0">AX</span>
           <BaseInput />
           <BaseInput />
         </div>
         <div class="flex items-center gap-x-4 mb-2">
-          <span>BX</span>
+          <span class="w-5 shrink-0">BX</span>
           <BaseInput />
           <BaseInput />
         </div>
         <div class="flex items-center gap-x-4 mb-2">
-          <span>CX</span>
+          <span class="w-5 shrink-0">CX</span>
           <BaseInput />
           <BaseInput />
         </div>
         <div class="flex items-center gap-x-4 mb-6">
-          <span>DX</span>
+          <span class="w-5 shrink-0">DX</span>
           <BaseInput />
           <BaseInput />
         </div>
-        <div class="grid grid-cols-3 gap-2 mb-6">
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-2 mb-6">
           <BaseButton :smaller="true">MOV AX, BX</BaseButton>
           <BaseButton :smaller="true">MOV AX, CX</BaseButton>
           <BaseButton :smaller="true">MOV AX, DX</BaseButton>
@@ -40,7 +40,7 @@
           <BaseButton :smaller="true">MOV DX, BX</BaseButton>
           <BaseButton :smaller="true">MOV DX, CX</BaseButton>
         </div>
-        <div class="grid grid-cols-3 gap-2">
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-2">
           <BaseButton :smaller="true">XCHG AX, BX</BaseButton>
           <BaseButton :smaller="true">XCHG AX, CX</BaseButton>
           <BaseButton :smaller="true">XCHG AX, DX</BaseButton>
@@ -62,4 +62,8 @@
 <script setup>
 import BaseButton from "./components/BaseButton.vue";
 import BaseInput from "./components/BaseInput.vue";
+
+function restart() {
+  console.log("restart");
+}
 </script>
--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="bg-blue-300 rounded-full" :class="smaller ? ['px-4', 'text-xs', 'py-1', 'uppercase'] : ['px-5', 'py-1.5', 'first-letter:uppercase']">
+  <button
+    type="button"
+    class="bg-blue-300 rounded-full hover:bg-black hover:text-white transition-colors duration-500"
+    :class="smaller ? ['px-4', 'text-xs', 'py-1', 'uppercase'] : ['px-5', 'py-1.5', 'first-letter:uppercase']"
+  >
     <slot />
   </button>
 </template>
